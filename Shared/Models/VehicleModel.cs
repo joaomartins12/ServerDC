cfb7275eb2f5617b82a6a02f87a4f3f9008e9e70
joaomartins12 <40610062+joaomartins12@users.noreply.github.com>
2026-08-19 23:26:16 +0100

    Execute vehicle writes before reading identity

--- Shared/Models/VehicleModel.cs
+++ Shared/Models/VehicleModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using MySql.Data.MySqlClient;
@@ -9,11 +9,6 @@ namespace Shared.Models
 {
     public class VehicleModel
     {
-        /// <summary>
-        /// Writes the vehicle to the database
-        /// </summary>
-        /// <param name="dbconn">The mysql connection</param>
-        /// <param name="vehicle">The vehicle that should be written</param>
         public static void Update(MySqlConnection dbconn, Vehicle vehicle)
         {
             using (var cmd = new UpdateCommand("UPDATE vehicles SET {0} WHERE CID=@vehId", dbconn))
@@ -21,19 +16,14 @@ namespace Shared.Models
                 cmd.AddParameter("@vehId", vehicle.CarId);
                 var updateCommand = cmd;
                 vehicle.WriteToDb(ref updateCommand);
+                cmd.Execute();
             }
         }
-        
-        /// <summary>
-        /// Retrieves a vehicle by id from the database
-        /// </summary>
-        /// <param name="dbconn">The mysql database connection</param>
-        /// <param name="carId">The Id of the vehicle to retrieve</param>
-        /// <returns>A new Vehicle class, null if none was found</returns>
+
         public static Vehicle Retrieve(MySqlConnection dbconn, uint carId)
         {
             var vehicle = new Vehicle();
-            
+
             var command = new MySqlCommand(
                 "SELECT * FROM Vehicles WHERE CID = @car", dbconn);
 
@@ -47,14 +37,7 @@ namespace Shared.Models
 
             return vehicle;
         }
-        
-        /// <summary>
-        /// Retrieves a list of vehicles from the specified character id
-        /// TODO: Shouldn't we move this to the CharacterModel?
-        /// </summary>
-        /// <param name="dbconn">The mysql database connection</param>
-        /// <param name="cid">The id of the character to retrieve vehicles from</param>
-        /// <returns>A List of Vehicle classes</returns>
+
         public static List<Vehicle> Retrieve(MySqlConnection dbconn, ulong cid)
         {
             var command = new MySqlCommand("SELECT * FROM Vehicles WHERE CharID = @cid", dbconn);
@@ -75,22 +58,24 @@ namespace Shared.Models
 
             return vehicles;
         }
-        
+
         public static long Create(MySqlConnection dbconn, Vehicle veh, ulong ownerId = 0)
         {
-            using (var cmd = new InsertCommand("INSERT INTO `vehicles` {0}", dbconn))
+            using (var cmd = new InsertCommand("INSERT INTO vehicles {0}", dbconn))
             {
-                if(ownerId != 0UL)
+                if (ownerId != 0UL)
                     cmd.Set("CharID", ownerId);
+
                 var insertCommand = cmd;
                 veh.WriteToDb(ref insertCommand);
+                cmd.Execute();
                 return cmd.LastId;
             }
         }
 
         public static bool Remove(MySqlConnection dbconn, ulong vehId)
         {
-            var command = new MySqlCommand("DELETE FROM `Vehicles` WHERE CID = @vehId", dbconn);
+            var command = new MySqlCommand("DELETE FROM Vehicles WHERE CID = @vehId", dbconn);
             command.Parameters.AddWithValue("@vehId", vehId);
             return command.ExecuteNonQuery() == 1;
         }
@@ -103,7 +88,6 @@ namespace Shared.Models
             using (var reader = command.ExecuteReader())
             {
                 if (!reader.Read()) return -1;
-                
                 return reader.GetInt32(0);
             }
         }
