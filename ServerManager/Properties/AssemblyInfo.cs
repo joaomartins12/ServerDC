d7f38401dbda6c47422294295aec6d71377c81fe
joaomartins12 <40610062+joaomartins12@users.noreply.github.com>
2026-08-20 00:10:08 +0100

    Add ServerManager assembly metadata

---
+++ ServerManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Drift City Server Manager")]
+[assembly: AssemblyDescription("Unified controller and log viewer for Drift City server processes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Drift City Server Manager")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a34a5080-3850-4be0-a6f1-14fab2916e36")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
