c732c7b21c2cfdfc2d1da8ccfd9d7470dd7af091
joaomartins12 <40610062+joaomartins12@users.noreply.github.com>
2026-08-20 01:31:41 +0100

    Send real active vehicle stats in StatUpdateAck

--- GameServer/Network/Handlers/CheckStat.cs
+++ GameServer/Network/Handlers/CheckStat.cs
@@ -1,5 +1,7 @@
-﻿using Shared.Network;
+﻿using GameServer.Util;
+using Shared.Network;
 using Shared.Network.GameServer;
+using Shared.Util;
 
 namespace GameServer.Network.Handlers
 {
@@ -8,86 +10,77 @@ namespace GameServer.Network.Handlers
         [Packet(Packets.CmdCheckStat)]
         public static void Handle(Packet packet)
         {
-            var ack = new CheckStatAnswer()
+            var character = packet.Sender.User == null ? null : packet.Sender.User.ActiveCharacter;
+            if (character == null)
             {
-                /*CarSpeed = 0,
-                CarDurability = 0,
-                CarAcceleration = 0,
-                CarBoost = 0,
+                Log.Warning("CmdCheckStat ignored: no active character.");
+                return;
+            }
 
-                PartSpeed = 0,
-                PartDurability = 0,
-                PartAcceleration = 0,
-                PartBoost = 0,
+            var activeCar = character.ActiveCar;
+            if (activeCar == null && character.GarageVehicles != null)
+                activeCar = character.GarageVehicles.Find(v => v != null && v.CarId == character.ActiveVehicleId);
 
-                UserSpeed = 0,
-                UserDurability = 0,
-                UserAcceleration = 0,
-                UserBoost = 0,
+            if (activeCar == null)
+            {
+                Log.Warning("CmdCheckStat: CID={0} has no active vehicle (ActiveVehicleId={1}).", character.Id, character.ActiveVehicleId);
+                packet.Sender.Send(new CheckStatAnswer().CreatePacket());
+                return;
+            }
 
-                CharSpeed = 0,
-                CharDurability = 0,
-                CharAcceleration = 0,
-                CharBoost = 0,
+            var stats = VehicleStatResolver.Resolve(activeCar);
+            if (stats == null)
+            {
+                Log.Warning("CmdCheckStat: unable to resolve stats for CID={0} CarId={1} CarType={2} Grade={3}.",
+                    character.Id, activeCar.CarId, activeCar.CarType, activeCar.Grade);
+                packet.Sender.Send(new CheckStatAnswer().CreatePacket());
+                return;
+            }
 
-                ItemUseSpeed = 0,
-                ItemUseCrash = 0,
-                ItemUseAcceleration = 0,
-                ItemUseBoost = 0,
+            // Parts are deliberately kept separate from the base vehicle values. Once CmdEquipItem/UnEquipItem
+            // is decoded, Equip* will contain the sum of the parts currently attached to this CarId.
+            var equipSpeed = 0;
+            var equipCrash = 0;
+            var equipAccel = 0;
+            var equipBoost = 0;
 
-                VehicleSpeed = 0,
-                VehicleDurability = 0,
-                VehicleAcceleration = 0,
-                VehicleBoost = 0,*/
-				// TODO: Nothing.
+            var ack = new CheckStatAnswer
+            {
+                BasedSpeed = stats.Speed,
+                BasedDurability = stats.Crash,
+                BasedAcceleration = stats.Accel,
+                BasedBoost = stats.Boost,
+
+                EquipSpeed = equipSpeed,
+                EquipDurability = equipCrash,
+                EquipAcceleration = equipAccel,
+                EquipBoost = equipBoost,
+
+                TotalSpeed = stats.Speed + equipSpeed,
+                TotalDurability = stats.Crash + equipCrash,
+                TotalAcceleration = stats.Accel + equipAccel,
+                TotalBoost = stats.Boost + equipBoost,
+
+                MitronCapacity = stats.MitronCapacity,
+                MitronEfficiency = stats.MitronEfficiency
             };
 
-            packet.Sender.Send(ack.CreatePacket());
-            /*
-            Send_StatUpdate:
-                XiStrStatInfo StatInfo;
-                XiStrEnChantBonus EnChantBonus;
-                
-            struct XiStrEnChantBonus
-{
-  int Speed;
-  int Crash;
-  int Accel;
-  int Boost;
-  float Drop;
-  float Exp;
-  float MitronCapacity;
-  float MitronEfficiency;
-};
+            Log.Info(
+                "StatUpdate: CID={0} CarDbId={1} VehicleId={2} Name={3} Grade=V{4} Source={5} Base[S={6},C={7},A={8},B={9}] Mitron[Capacity={10},Efficiency={11}]",
+                character.Id,
+                activeCar.CarId,
+                stats.VehicleId,
+                string.IsNullOrWhiteSpace(stats.VehicleName) ? "UNKNOWN" : stats.VehicleName,
+                stats.Grade,
+                stats.Source,
+                stats.Speed,
+                stats.Crash,
+                stats.Accel,
+                stats.Boost,
+                stats.MitronCapacity,
+                stats.MitronEfficiency);
 
-struct XiStrStatInfo
-{
-  int BasedSpeed;
-  int BasedCrash;
-  int BasedAccel;
-  int BasedBoost;
-  int EquipSpeed;
-  int EquipCrash;
-  int EquipAccel;
-  int EquipBoost;
-  int CharSpeed;
-  int CharCrash;
-  int CharAccel;
-  int CharBoost;
-  int ItemUseSpeed;
-  int ItemUseCrash;
-  int ItemUseAccel;
-  int ItemUseBoost;
-  int TotalSpeed;
-  int TotalCrash;
-  int TotalAccel;
-  int TotalBoost;
-};
-            
-            XiCsCharInfo::StatUpdate(pCharInfo);
-            PacketSend::Send_StatUpdate(lpDispatch);
-            PacketSend::Send_PartyEnChantUpdateAll(lpDispatch); // TODO
-            */
+            packet.Sender.Send(ack.CreatePacket());
         }
     }
 }
