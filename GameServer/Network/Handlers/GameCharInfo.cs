e845fd4f3aaf340d66bc62cc95a428e48c071b63
joaomartins12 <40610062+joaomartins12@users.noreply.github.com>
2026-08-20 01:37:45 +0100

    Populate GameCharInfo vehicle statistics from catalog

--- GameServer/Network/Handlers/GameCharInfo.cs
+++ GameServer/Network/Handlers/GameCharInfo.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Models;
+﻿using GameServer.Util;
+using Shared.Models;
 using Shared.Network;
 using Shared.Network.GameServer;
 using Shared.Objects;
@@ -9,46 +10,75 @@ namespace GameServer.Network.Handlers
     public class GameCharInfo
     {
         [Packet(Packets.CmdGameCharInfo)]
-        public static void Handle(Packet packet) // TODO: Send data correspoding to the charname, not user
+        public static void Handle(Packet packet) // TODO: Send data corresponding to the charname, not user
         {
-            /*
-            [Debug] - GameCharInfo: 000000: 41 00 64 00 6D 00 69 00 6E 00 69 00 73 00 74 00  A · d · m · i · n · i · s · t ·
-            000016: 72 00 61 00 74 00 6F 00 72 00 00 00 0A 00 06 00  r · a · t · o · r · · · · · · ·
-            000032: 0D 25 33 65 63 65 69 76 65 64 00 00 00 00  · % 3 e c e i v e d · · · ·
-
-            [Info] - Received GameCharInfo (id 660, 0x294) on 11021.
-
-            Wrong Packet Size. CMD(661) CmdLen: : 1177, AnalysisSize: 831
-            // We're missing 346 bytes of data.
-            */
             var gameCharInfoPacket = new GameCharInfoPacket(packet);
 
-            // TODO: Combine this into 1 MySQL Query.
             var character = CharacterModel.Retrieve(GameServer.Instance.Database.Connection,
                 gameCharInfoPacket.CharacterName);
             if (character == null)
             {
-                Log.Error($"Character {gameCharInfoPacket.CharacterName} was not found in DB!");
+                Log.Error("Character {0} was not found in DB!", gameCharInfoPacket.CharacterName);
                 packet.Sender.SendDebugError("Character not found");
 #if !DEBUG
                 packet.Sender.KillConnection("Character for CmdGameCharInfo not found");
 #endif
                 return;
             }
+
             var user = AccountModel.Retrieve(GameServer.Instance.Database.Connection, character.Uid);
-            
+            var statisticInfo = BuildStatisticInfo(character.ActiveCar);
+
             var ack = new GameCharInfoAnswer
             {
                 Character = character,
                 Vehicle = character.ActiveCar,
-                StatisticInfo = new XiStrStatInfo(),
+                StatisticInfo = statisticInfo,
                 Crew = character.Crew,
                 Serial = user.VehicleSerial,
-                //LocType = 'A',
-                ChId = (char)character.LastChannel, //'A',
-                //LocId = 1
+                ChId = (char)character.LastChannel
             };
+
+            if (character.ActiveCar != null)
+            {
+                var resolved = VehicleStatResolver.Resolve(character.ActiveCar);
+                if (resolved != null)
+                {
+                    Log.Info(
+                        "GameCharInfo stats: CID={0} CarDbId={1} VehicleId={2} Name={3} Grade=V{4} Source={5} Base[S={6},C={7},A={8},B={9}]",
+                        character.Id,
+                        character.ActiveCar.CarId,
+                        resolved.VehicleId,
+                        resolved.VehicleName ?? "UNKNOWN",
+                        resolved.Grade,
+                        resolved.Source ?? "UNKNOWN",
+                        resolved.Speed,
+                        resolved.Crash,
+                        resolved.Accel,
+                        resolved.Boost);
+                }
+            }
+
             packet.Sender.Send(ack.CreatePacket());
+        }
+
+        private static XiStrStatInfo BuildStatisticInfo(Vehicle vehicle)
+        {
+            var info = new XiStrStatInfo();
+            var resolved = VehicleStatResolver.Resolve(vehicle);
+            if (resolved == null)
+                return info;
+
+            info.BasedSpeed = resolved.Speed;
+            info.BasedCrash = resolved.Crash;
+            info.BasedAccel = resolved.Accel;
+            info.BasedBoost = resolved.Boost;
+
+            info.TotalSpeed = resolved.Speed;
+            info.TotalCrash = resolved.Crash;
+            info.TotalAccel = resolved.Accel;
+            info.TotalBoost = resolved.Boost;
+            return info;
         }
     }
 }
