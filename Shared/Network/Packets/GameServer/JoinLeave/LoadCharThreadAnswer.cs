aaf7a0a6402b0ec54063c03c2c57a94253eb62dd
joaomartins12 <40610062+joaomartins12@users.noreply.github.com>
2026-08-21 19:43:30 +0100

    Unify vehicle paint across lobby and world snapshots

--- Shared/Network/Packets/GameServer/JoinLeave/LoadCharThreadAnswer.cs
+++ Shared/Network/Packets/GameServer/JoinLeave/LoadCharThreadAnswer.cs
@@ -14,7 +14,6 @@ namespace Shared.Network.GameServer
         public uint ServerStartTime;
         public Character Character = new Character();
         public Vehicle[] Vehicles = new Vehicle[0];
-
         public int CurrentCarId;
 
         public override Packet CreatePacket()
@@ -24,34 +23,45 @@ namespace Shared.Network.GameServer
 
         public override int ExpectedSize() => (50 * Vehicles.Length - 1) + 385;
 
+        private static uint GetClientColor(Vehicle vehicle)
+        {
+            if (vehicle == null) return 0;
+            return vehicle.Color != 0 ? vehicle.Color : vehicle.BaseColor;
+        }
+
         public override byte[] GetBytes()
         {
             using (var ms = new MemoryStream())
+            using (var bs = new BinaryWriterExt(ms))
             {
-                using (var bs = new BinaryWriterExt(ms))
+                bs.Write(ServerId);
+                bs.Write(ServerStartTime);
+                Character.Serialize(bs);
+                bs.Write(Vehicles.Length);
+                foreach (var vehicle in Vehicles)
                 {
-                    bs.Write(ServerId);
-                    bs.Write(ServerStartTime);
-                    Character.Serialize(bs);
-                    bs.Write(Vehicles.Length);
-                    foreach (var vehicle in Vehicles)
-                    {
-                        bs.Write(vehicle.CarId);
-                        bs.Write(vehicle.CarType);
-                        bs.Write(vehicle.BaseColor);
-                        bs.Write(vehicle.Grade);
-                        bs.Write(vehicle.SlotType);
-                        bs.Write(vehicle.AuctionCnt);
-                        bs.Write(vehicle.Mitron);
-                        bs.Write(vehicle.Kmh);
+                    var car = vehicle ?? new Vehicle();
+                    var clientColor = GetClientColor(car);
 
-                        bs.Write(vehicle.Color);
-                        bs.Write(vehicle.Color2);
-                        bs.Write(vehicle.MitronCapacity);
-                        bs.Write(vehicle.MitronEfficiency);
-                        bs.Write(vehicle.AuctionOn);
-                        bs.Write(vehicle.SBBOn);
-                    }
+                    bs.Write(car.CarId);
+                    bs.Write(car.CarType);
+
+                    // v0.77a seeds its garage/world render cache from the first colour
+                    // DWORD in XiStrCarInfo. Keep the persistent BaseColor untouched in
+                    // the model/DB, but put the current effective paint on the wire here.
+                    bs.Write(clientColor);
+
+                    bs.Write(car.Grade);
+                    bs.Write(car.SlotType);
+                    bs.Write(car.AuctionCnt);
+                    bs.Write(car.Mitron);
+                    bs.Write(car.Kmh);
+                    bs.Write(clientColor);
+                    bs.Write(car.Color2);
+                    bs.Write(car.MitronCapacity);
+                    bs.Write(car.MitronEfficiency);
+                    bs.Write(car.AuctionOn);
+                    bs.Write(car.SBBOn);
                 }
                 return ms.ToArray();
             }
--- Shared/Network/Packets/LobbyServer/Outgoing/UserInfoAnswerPacket.cs
+++ Shared/Network/Packets/LobbyServer/Outgoing/UserInfoAnswerPacket.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using Shared.Objects;
 using Shared.Util;
 
@@ -9,20 +9,8 @@ namespace Shared.Network.LobbyServer
     /// </summary>
     public class UserInfoAnswerPacket : OutPacket
     {
-        /// <summary>
-        ///     The characters the user has
-        /// </summary>
         public Character[] Characters;
-
-        /// <summary>
-        ///     The permissions the user has
-        /// </summary>
         public int Permissions;
-
-        /// <summary>
-        ///     The username
-        /// </summary>
-        /// <remarks>Always 18</remarks>
         public string Username;
 
         public UserInfoAnswerPacket()
@@ -37,29 +25,64 @@ namespace Shared.Network.LobbyServer
             return base.CreatePacket(Packets.UserInfoAck);
         }
 
-        public override int ExpectedSize() => (120*(Characters.Length-1)) + 194; // 194 => 74 + 120!! 
-        
+        public override int ExpectedSize() => (120 * (Characters.Length - 1)) + 194;
+
+        private static uint GetClientColor(Vehicle vehicle)
+        {
+            if (vehicle == null) return 0;
+            return vehicle.Color != 0 ? vehicle.Color : vehicle.BaseColor;
+        }
+
+        private static void WriteCharacterShort(BinaryWriterExt bs, Character character)
+        {
+            var car = character.ActiveCar ?? new Vehicle();
+
+            bs.WriteUnicodeStatic(character.Name, 21);
+            bs.Write(character.Id);
+            bs.Write((int)character.Avatar);
+            bs.Write((int)character.Level);
+            bs.Write(character.ActiveVehicleId);
+            bs.Write(car.CarType);
+
+            // Character Select is one of the client-side roots for the current car.
+            // Retail uses this colour while constructing the preview/world car cache.
+            // Sending the immutable catalogue BaseColor here made a visually-painted car
+            // fall back to the model default after returning to Server/Character Select.
+            bs.Write(GetClientColor(car));
+
+            bs.Write(character.CreationDate);
+            bs.Write(character.CrewId);
+            if (character.Crew != null)
+            {
+                bs.Write(character.Crew.MarkId);
+                bs.WriteUnicodeStatic(character.Crew.Name, 13);
+                bs.Write((short)character.CrewRank);
+            }
+            else
+            {
+                bs.Write(0L);
+                bs.WriteUnicodeStatic("", 13);
+                bs.Write((short)0);
+            }
+            bs.Write(character.Guild);
+        }
+
         public override byte[] GetBytes()
         {
             using (var ms = new MemoryStream())
+            using (var bs = new BinaryWriterExt(ms))
             {
-                using (var bs = new BinaryWriterExt(ms))
-                {
-                    bs.Write(Permissions);
-                    //bs.Write(CharacterCount);
-                    bs.Write(Characters.Length);
-                    bs.WriteUnicodeStatic(Username, 18);
-                    bs.Write((long) 0);
-                    bs.Write((long) 0);
-                    bs.Write((long) 0);
-                    bs.Write(0);
-                    // --- 74 --- //
+                bs.Write(Permissions);
+                bs.Write(Characters.Length);
+                bs.WriteUnicodeStatic(Username, 18);
+                bs.Write((long)0);
+                bs.Write((long)0);
+                bs.Write((long)0);
+                bs.Write(0);
 
-                    foreach (var character in Characters)
-                    {
-                        character.SerializeShort(bs);
-                    }
-                }
+                foreach (var character in Characters)
+                    WriteCharacterShort(bs, character ?? new Character());
+
                 return ms.ToArray();
             }
         }
