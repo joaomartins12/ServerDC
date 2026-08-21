d97d16bac5ae22341f27ae7dc304303ef5c16388
joaomartins12 <40610062+joaomartins12@users.noreply.github.com>
2026-08-21 17:34:46 +0100

    Support live PlayerInfoRes visual updates

--- Shared/Network/Packets/GameServer/Info/PlayerInfoOldAnswer.cs
+++ Shared/Network/Packets/GameServer/Info/PlayerInfoOldAnswer.cs
@@ -6,12 +6,19 @@ namespace Shared.Network.GameServer
 {
     public class PlayerInfoOldAnswer : OutPacket
     {
+        public const ushort PlayerInfoOldPacketId = 802;
+        public const ushort PlayerInfoLivePacketId = 809;
+
         public XiPlayerInfo PlayerInfo = new XiPlayerInfo();
         public XiPlayerInfo[] PlayerInfos = new XiPlayerInfo[0];
+
+        // Runtime visual refreshes use the retail PlayerInfoRes path (809 / 0x329).
+        // Initial discovery/request-response callers can explicitly select 802.
+        public ushort PacketId = PlayerInfoLivePacketId;
         
         public override Packet CreatePacket()
         {
-            return base.CreatePacket(Packets.PlayerInfoOldAck);
+            return base.CreatePacket(PacketId);
         }
 
         public override int ExpectedSize() => (216 * PlayerInfos.Length) + 222;
@@ -22,7 +29,7 @@ namespace Shared.Network.GameServer
             {
                 using (var bs = new BinaryWriterExt(ms))
                 {
-                    bs.Write(PlayerInfos.Length+1);
+                    bs.Write(PlayerInfos.Length + 1);
                     bs.Write(PlayerInfo);
                     foreach (var playerInfo in PlayerInfos)
                     {
